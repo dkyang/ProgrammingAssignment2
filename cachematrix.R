1a85c0d838c837b1f0b4e3a3eab0ef1d89a72606
dkyang <yangzhuangsuan@gmail.com>
2014-04-27 15:54:15 +0800

    finish cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions just mimic the example function "makeVector",
+## you can use "makeCacheMatrix" to build a "matrix object" which can carry 
+## its inversion. At first, its inversion is null. When you use "cacheSolve" to
+## the matrix object for the first time, its inversion will be actually computed 
+## and stored in the matrix object. When you use "cacheSolve" for the second time,
+## its inversion will not be computed and will be retrived from the object since
+## the inversion has been cached.
 
-## Write a short comment describing this function
+## This function just like a "construction function" in OOP.It takes a matrix object 
+## as arguments, and return a "matrix object" which can cache its inversion.
+## The returned "matrix object" is actually a list, and has several functions as
+## its elements.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat_inv <- NULL
+  set <- function(y) {
+    x <<- y
+    mat_inv <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) mat_inv <<- inverse
+  get_inverse <- function() mat_inv
+  list(set = set, get = get, 
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
 
+## This function takes a "matrix object" build by "makeCacheMatrix".
+## If the "matrix object" has cached inversion, it will simply return it.
+## If the "matrix object" hasn't cached inversion, it will actually compute 
+## it and store the inversion in the "matrix object".
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat_inv <- x$get_inverse()
+  if(!is.null(mat_inv)) {
+    message("getting cached data")
+    return(mat_inv)
+  }
+  data <- x$get()
+  mat_inv <- solve(data)
+  x$set_inverse(mat_inv)
+  mat_inv
 }
